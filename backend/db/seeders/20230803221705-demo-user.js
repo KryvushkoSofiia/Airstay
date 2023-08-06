60200ab1f339c1f47996f4dde200d8ab68fed875
sofiiakryvushko <sofiiakryvushko@hmail.com>
2023-08-05 20:19:47 -0500

    Add firstName and lastName

--- backend/db/seeders/20230803221705-demo-user.js
+++ backend/db/seeders/20230803221705-demo-user.js
@@ -13,18 +13,24 @@ module.exports = {
     await User.bulkCreate([
       {
         email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+          username: 'Demo-lition',
+          firstName: 'Demo',
+          lastName: 'User',
+          hashedPassword: bcrypt.hashSync('password'),
       },
       {
         email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+          username: 'FakeUser1',
+          firstName: 'John',
+          lastName: 'Doe',
+          hashedPassword: bcrypt.hashSync('password2'),
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        firstName: 'Jane',
+        lastName: 'Smith',
+        hashedPassword: bcrypt.hashSync('password3'),
       }
     ], { validate: true });
   },
