fcfda15179b87bcb67edca2823e3f78182489d65
sofiiakryvushko <sofiiakryvushko@hmail.com>
2023-08-11 11:35:06 -0500

    changed session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -14,17 +14,8 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
-<<<<<<< HEAD
 // backend/routes/api/session.js
 // ...
-=======
-<<<<<<< HEAD
-// backend/routes/api/session.js
-// ...
-=======
-router.use(restoreUser);
->>>>>>> 73aff261035d0a369628ec78e6803747970fc554
->>>>>>> dev
 
 const validateLogin = [
   check("credential")
@@ -37,26 +28,12 @@ const validateLogin = [
   handleValidationErrors,
 ];
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> dev
 // backend/routes/api/session.js
 // ...
 
 // Log in
 router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
-<<<<<<< HEAD
-=======
-=======
-
-router.post(
-  '/', validateLogin,
-  async (req, res, next) => {
-    const { credential, password } = req.body;
->>>>>>> 73aff261035d0a369628ec78e6803747970fc554
->>>>>>> dev
 
   const user = await User.unscoped().findOne({
     where: {
@@ -117,64 +94,9 @@ router.get("/", (req, res) => {
     };
     return res.json({
       user: safeUser,
-
     });
-<<<<<<< HEAD
-  }});
-  
-=======
-
-  }
-}
-);
-
-// Restore session user
-
-router.get(
-  '/',
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-      return res.json({
-        user: safeUser
-      });
-    } else return res.json({ user: null });
-  }
-);
->>>>>>> dev
-
-// Restore session user
-
-router.get(
-  '/',
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-      return res.json({
-        user: safeUser
-      });
-    } else return res.json({ user: null });
-  }
-);
-
-// Log out
-router.delete(
-  '/',
-  (_req, res) => {
-    res.clearCookie('token');
-    return res.json({ message: 'success' });
-  }
-);
+  } else return res.json({ user: null });
+});
 
 // ...
 
